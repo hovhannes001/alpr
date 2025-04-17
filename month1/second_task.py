1cf481aa9c4467cdc1b4842127c854199d607f01
hovhannes001 <hovhannesbaghdasaryan17@gmail.com>
2025-04-17 17:56:30 +0400

    message

--- month1/second_task.py
+++ month1/second_task.py
@@ -5,14 +5,53 @@ custom_path = Path("./image_and_videos").resolve()
 sys.path.append(str(custom_path))
 
  	
-video = cv2.VideoCapture(0)
-fpn = video.get(7)
+# video = cv2.VideoCapture("traffic.webm")
+# fps = video.get(7)
+# print(fps)
+# while(video.isOpened()):
+#     ret, frame = video.read()
+#     if ret == True:
+#         cv2.imshow("frame",frame)
+#         key = cv2.waitKey(1)
 
+#         if key == ord('q'):
+#             break
+#     else:
+#         break
+    
+# video.release()
+# cv2.destroyAllWindows()
+
+#video write
+
+video2 = cv2.VideoCapture("video.mp4")
+
+frame_width = int(video2.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(video2.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frame_size = (frame_height , frame_width)
+
+fps2 = video2.get(7)
+forcc = cv2.VideoWriter_fourcc(*"XVID")
+output = cv2.VideoWriter("record_camera.avi", cv2.VideoWriter_fourcc(*"XVID"), fps2,(frame_width, frame_height))  
+
+while(video2.isOpened()):
+    ret, frame = video2.read()
+    if ret == True:
+        output.write(frame)
+        key = cv2.waitKey(1)
+        cv2.imshow("f",frame)
+        if(key == ord('q')):
+            break
+    else:
+        break
+
+# video.release()
+video = cv2.VideoCapture("record_camera.avi")
 while(video.isOpened()):
     ret, frame = video.read()
     if ret == True:
         cv2.imshow("frame",frame)
-        key = cv2.waitKey(100)
+        key = cv2.waitKey(1)
 
         if key == ord('q'):
             break
