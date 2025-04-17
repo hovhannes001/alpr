5ba87848357ec2e5ee48aa3908d739f89d12effa
hovhannes001 <hovhannesbaghdasaryan17@gmail.com>
2025-04-17 16:00:47 +0400

    some changes

--- month1/first_task.py
+++ month1/first_task.py
@@ -1,15 +1,15 @@
 import cv2
 import matplotlib.pyplot as plt
-img = cv2.imread("./logo.png")
+img = cv2.imread("./../images_and_videos/logo.png")
 print(type(img))
 total_number_of_elements = img.shape
 dimensions = img.size
 
-img2 = cv2.imread("roi.jpg")
+img2 = cv2.imread("./../images_and_videos/roi.jpg")
 # img[6,6] = (0,0,0)
 # cv2.imshow("messi",img)  
 # cv2.waitKey(0)
-gray_img = cv2.imread("logo.png",cv2.IMREAD_GRAYSCALE)
+gray_img = cv2.imread("./../images_and_videos/logo.png",cv2.IMREAD_GRAYSCALE)
 dimensions_gray = gray_img.shape
 gray_img_size = gray_img.size
 b,g,r = cv2.split(img)
@@ -19,5 +19,5 @@ plt.imshow(img)
 plt.subplot(122)
 plt.imshow(mathplotlib_img)
 plt.show()
-cv2.imwrite("test.jpg",mathplotlib_img)
+cv2.imwrite("./../images_and_videos/test.jpg",mathplotlib_img)
 cv2.destroyAllWindows()
